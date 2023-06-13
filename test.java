22182f8a297cfbf73eb68aa109c894d16b24ac90
Bastien Fournier <116389488+BastienFournier@users.noreply.github.com>
2023-06-13 13:47:45 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,35 +1,29 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
+
 public class test extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
+
+        Label label = new Label("Hello, World!");
+
+
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(label);
 
- Scene scene = new Scene(root, 300, 250);
 
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(root, 400, 300);
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello World");
         primaryStage.show();
     }
- public static void main(String[] args) {
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
